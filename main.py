80073b25f1f6d550234c18d7947a180d57f1f355
vunturi <45012495+vunturi@users.noreply.github.com>
2019-06-24 17:04:12 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,98 +1,71 @@
-
 import os
 import csv
 
-# Load file
-csv_file = os.path.join(r"C:\Users\vuntu\Desktop\HomeWork-Python\Python-Challange\PyPoll\election_data.csv")
+#File to load
+csvFile = os.path.join("C:\\Users\\vuntu\Desktop\\HomeWork-Python\\Python-Challange\\PyBank\\budget_data.csv")
 
 #Output file
-txt_file = os.path.join(r"C:\Users\vuntu\Desktop\HomeWork-Python\Python-Challange\PyPoll\electionResultFile.txt")
+txtFile = os.path.join("PyBank", "analyzed_budget.txt")
 
-#Total Votes Counter
-total_votes = 0
+#Financial data to be analyzed:
+monthCount = 0
+totalNet = 0
+monthChange = []
+netPLChange = []
+greatestIncreaseP = [" ", 0]
+greatestDecreaseL = [" ", 0]
 
-# Vote counters and candidate list
-candidate_votes = {}
-candidate_list = []
+#Read CSV file and convert into list of dictionaries
+with open(csvFile) as performData:
+	csvReader = csv.reader(performData)
 
-# Winning Candidate and Winning Count Tracker
-winning_cand = ""
-winning_count = 0
+	#Read header row
+	header = next(csvReader)
 
-# Read csv file
-with open(csv_file) as election_data:
-    csv_reader = csv.reader(election_data)
+	#Extract first row and avoid append Profit and Loss change
+	firstRow = next(csvReader)
+	monthCount = monthCount + 1
+	totalNet = totalNet + int(firstRow[1])
+	prevNet = int(firstRow[1])
+		
+	for row in csvReader:
 
-    #Read header
-    header = next(csv_reader) 
+		#Track total
+		monthCount = monthCount + 1
+		totalNet = totalNet + int(row[1])
 
-    #For each row---
-    for row in csv_reader:
 
-     # Run the loader animation
-        print(". ", end=""),
+		#Track monthChange
+		netChange = int(row[1]) - prevNet
+		prevNet = int(row[1])
+		netPLChange = netPLChange + [netChange]
+		monthChange = monthChange + [row[0]]
 
-        # Add to the total vote count
-        total_votes = total_votes + 1
+		#Greatest Increase
+		if netChange > greatestIncreaseP[1]:
+			greatestIncreaseP[0] = row[0]
+			greatestIncreaseP[1] = netChange
 
-        # Extracting candidate name from each row....
-        candidate_name = row[2]
+		#Greatest Decrease
+		if netChange < greatestDecreaseL[1]:
+			greatestDecreaseL[0] = row[0]
+			greatestDecreaseL[1] = netChange
 
-        # If the candidate does not match any existing candidate
-        if candidate_name not in candidate_list:
+#Average netChange
+netMonthlyAve = sum(netPLChange) / len(netPLChange)
 
-            # Add it to the list of running candidate
-            candidate_list.append(candidate_name)
+#Create summary txtFile
+analysis = (
+f"\nPerformance Analysis\n"
+f"---------------------\n"
+f"Total Months: {monthCount}\n"
+f"Total: ${totalNet}\n"
+f"Average Change: ${netMonthlyAve: .2f}\n"
+f"Greatest Increase in Profits: {greatestIncreaseP[0]} (${greatestIncreaseP[1]})\n"
+f"Greatest Decrease in Profits: {greatestDecreaseL[0]} (${greatestDecreaseL[1]})\n")
 
-            # Start candidate's voting count
-            candidate_votes[candidate_name] = 0
-        
-        candidate_votes[candidate_name] = candidate_votes[candidate_name] + 1
+print(analysis)
 
-    # Then add a vote to that candidate's count
-
-# Print the results and export the data to text file
-
-with open(txt_file, "w") as txtfile:
-
-    # Print the final vote count
-    election_results = (
-        f"\n\nElection Results\n"
-        f"Total Votes: {total_votes}\n"
-        f"-------------------------\n")
-    print(election_results, end="")
-
-    # Saving final num to the text file
-    txtfile.write(election_results)
-
-    # Determine the winner
-    for candidate in candidate_votes:
-
-        # Retrieve vote count and %
-        votes = candidate_votes.get(candidate)
-        vote_percent = float(votes) / float(total_votes) * 100
-
-        # Determine winning vote count and candidate
-        if (votes > winning_count):
-            winning_count = votes
-            winning_cand = candidate
-
-        # Print each candidate's voter count and %
-        voter_output = f"{candidate}: {vote_percent:.3f}% ({votes})\n"
-        print(voter_output, end="")
-
-        # Save each candidate's voter count and percentage to text file
-        txtfile.write(voter_output)
-
-    # Print the winning candidate (to terminal)
-    winning_candidate_summary = (
-        f"-------------------------\n"
-        f"Winner: {winning_cand}\n"
-        f"-------------------------\n")
-
-    print(winning_candidate_summary)
-    txtfile.write(winning_candidate_summary)
-
-    # Save the winning candidate's name to the text file
-    #txtfile.write("C:\\Users\\vuntu\\Desktop\\HomeWork-Python\\Python-Challange\\PyPoll\\electionResultFile.txt")
-
+with open("C:\\Users\\vuntu\Desktop\\HomeWork-Python\Python-Challange\\PyBank\\analyzed_budget.txt", "w") as txt_file:
+	 txt_file.write(analysis)
+		
